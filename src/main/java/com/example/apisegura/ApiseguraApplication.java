0282d58edb048cb6d5db2573a0021319216e12f5
Giuliano Espejo Mezzabotta <129322090+Giuliano-Espejo@users.noreply.github.com>
2024-05-30 11:28:22 -0300

    first commit

---
+++ src/main/java/com/example/apisegura/ApiseguraApplication.java
@@ -0,0 +1,13 @@
+package com.example.apisegura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiseguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiseguraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apisegura/ApiseguraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apisegura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiseguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
